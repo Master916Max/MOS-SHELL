1a6c9c85c2329a5d4a2089405c773af8e07404f5
Max Hammer <hma52366@outlook.de>
2025-08-14 17:44:29 +0200

    Adding Background Functional

---
+++ Theme_SYS/Background.py
@@ -0,0 +1,14 @@
+import Definds
+
+class Background:
+    def __init__(self, mode, colors: list[tuple[int]] = None, file = None):
+        self.mode = mode
+        self.colors = colors
+        pass
+
+    def render_bg(screen):
+
+        return screen
+    
+    def update(setting):
+        pass
